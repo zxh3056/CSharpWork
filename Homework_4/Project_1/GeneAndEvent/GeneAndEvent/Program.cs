bab3064d43a71432dd9d6960a90d8dccf9a56378
zxh3056 <798186756@qq.com>
2020-03-15 15:12:48 +0800

    the forth homework

---
+++ Homework_4/Project_1/GeneAndEvent/GeneAndEvent/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneAndEvent
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            head = null;
+            tail = null;
+        }
+        public Node<T> Head { get => head; }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void Foreach(Action<T> act)
+        {
+            Node<T> n = head;
+            while(n != null)
+            {
+                act(n.Data);
+                n = n.Next;
+            }
+        }
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            GenericList<int> list_1 = new GenericList<int>();
+            int sum = 0; int max = 0; int min = int.MaxValue;
+            for(int i = 0; i <= 10; i++)
+            {
+                list_1.Add(i);
+            }
+            list_1.Foreach(m => sum += m);//这个Lambda表达式对应act委托，m就是act委托的参数，所以m就对应于n.Data
+            Console.WriteLine(sum);
+            list_1.Foreach((m) => max = m > max ? m : max);
+            Console.WriteLine(max);
+            list_1.Foreach(m => min = m < min ? m : min);
+            Console.WriteLine(min);
+        }
+    }
+}
+   
+
---
+++ Homework_4/Project_2/EventTry/EventTry/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace EventTry
+{
+    
+    class Clock
+    {
+        public delegate void ClockAlarm();
+        public delegate void ClockTick();
+        public event ClockAlarm clockAlarm;
+        public event ClockTick clockTick;
+        public int alarmHour, alarmMinute, alarmSecond = 0;
+        public void SetAlarm(int hour,int minute,int second)
+        {
+            alarmHour = hour;
+            alarmMinute = minute;
+            alarmSecond = second;
+        }
+        public void Tick()
+        {
+            clockTick();
+        }
+        public void Alarm()
+        {
+            clockAlarm();
+        }
+    }
+    class ClockStart//闹钟的启动程序
+    {
+       public Clock clock = new Clock();
+       
+        public void Start()
+        {
+          
+            while (true)
+            {
+                clock.Tick();
+                int hour = DateTime.Now.Hour;
+                int minute = DateTime.Now.Minute;
+                int second = DateTime.Now.Second;
+                if (clock.alarmHour == hour && clock.alarmMinute == minute && clock.alarmSecond == second)
+                {
+                    clock.Alarm();
+                }
+                Thread.Sleep(1000);
+            }
+            
+        }
+      
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClockStart clockstart = new ClockStart();
+            clockstart.clock.clockAlarm += Alarming;
+            clockstart.clock.clockTick += Ticking;
+            clockstart.clock.SetAlarm(15, 10, 30);
+            clockstart.Start();
+        }
+        public static void Alarming()
+        {
+            Console.WriteLine("闹钟响了！！！！！！！");
+        }
+        public static void Ticking()
+        {
+            Console.WriteLine("滴答");
+        }
+    }
+}
